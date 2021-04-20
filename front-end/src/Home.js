2e0d5b551733dde5f83066e2f35ff62ad1fbe7b2
Denuwan <denuwan.2018583@iit.ac.lk>
2021-04-20 12:32:20 +0530

    Pricing page added

--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -10,77 +10,72 @@ function Home(){
     return(
         <div className="content">
           <nav className="navbar">
-        <div className="navContainer">
+            <div className="navContainer">
           
 
-          <ul className={"nav-menu"}>
-            <li className="nav-item">
-              <NavLink
-                exact
-                to="/"
-                className="nav-links"
-              >
-                Home
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                exact
-                to="/about"
-                className="nav-links"
-              >
-                About
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                exact
-                to="/blog"
-                className="nav-links"
-              >
-                Gazette
-              </NavLink>
-            </li>
+              <ul className={"nav-menu"}>
+                <li className="nav-item">
+                  <NavLink
+                    exact
+                    to="/"
+                    className="nav-links"
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    exact
+                    to="/about"
+                    className="nav-links"
+                  >
+                    About
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    exact
+                    to="/blog"
+                    className="nav-links"
+                  >
+                    Gazette
+                  </NavLink>
+                </li>
            
-          </ul>
+              </ul>
         
-        </div>
-      </nav>
+            </div>
+          </nav>
 
            
 
-        <div className="nav">
+          <div className="nav">
 
-          <div className="nav-logo"> 
-            <h1 className ="AppName">Coco Measure</h1>
-            <h3 className="Moto">Measure before you buy it...</h3>
-          </div> 
+            <div className="nav-logo"> 
+              <h1 className ="AppName">Coco Measure</h1>
+              <h3 className="Moto">Measure before you buy it...</h3>
+            </div> 
 
        
 
-        </div>
+            </div>
 
-        <div className = "buttonContainer">        
-          <div className = "button1">
-            <button className="btn"><Link to="/Upload">Upload an Image</Link></button>
-          </div>
+            <div className = "buttonContainer">        
+              <div className = "button1">
+                <button className="btn"><Link to="/Upload">Upload an Image</Link></button>
+              </div>
         
-          <div className = "button2">
-            <button className="btn"><Link to="/camera">Capture an Image</Link></button>
+            <div className = "button2">
+              <button className="btn"><Link to="/camera">Capture an Image</Link></button>
+            </div>
+
+            <div className = "button3">
+              <button className="btn"><Link to="/Pricing">View Price Categories </Link></button>
+            </div>
           </div>
 
-          <div className = "button3">
-            <button className="btn"><Link to="/Pricing">View Price Categories </Link></button>
-          </div>
         </div>
-
-        {/* <button className="btn">Capture Photo</button> */}
-
-
-
-
-      </div>
-  );
+    );
 }
 
 export default Home;
--- front-end/src/Pricing.js
+++ front-end/src/Pricing.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './Pricing.css';
 
 
 import { BrowserRouter as Router, Route, Link, Navlink, Switch } from 'react-router-dom';
@@ -7,25 +8,31 @@ function Pricing() {
   return (
     <div className="priceCategories">
         <table id="customers">
-            <tr>
-                <th>Size</th>
-                <th>Price</th>
-            </tr>
+            <div className="pricingpage"> 
+                <h1 className ="pricingpagename">Coco Measure Pricie List</h1>
+                <h3 className="pricingdescription">Prices According to the Consumer Authority Gazzete</h3>
+            </div>
+            <div className = "table">
+                <tr>
+                    <th id="tableheadsize">Size</th>
+                    <th id="tableheadprice">Price</th>
+                </tr>
 
-            <tr>
-                <td>Below 12 inches</td>
-                <td>Rs 60/=</td>
-            </tr>
+                <tr>
+                    <td id="tablecat">Below 12 inches</td>
+                    <td id="tableprice">Rs 60/=</td>
+                </tr>
 
-            <tr>
-                <td>Between 12 and 13 inches</td>
-                <td>Rs 65/=</td>
-            </tr>
+                <tr>
+                    <td id="tablecat">Between 12 and 13 inches</td>
+                    <td id="tableprice">Rs 65/=</td>
+                </tr>
 
-            <tr>
-                <td>Above 13 inches</td>
-                <td>Rs 70/=</td>
-            </tr>
+                <tr>
+                    <td id="tablecat">Above 13 inches</td>
+                    <td id="tableprice">Rs 70/=</td>
+                </tr>
+            </div>
 
         </table>
 
