b7550543ee0749a4efc6dc135b419cea107bb943
manodya99 <manodya.2018629@iit.ac.lk>
2021-04-21 15:11:42 +0530

    add- css for about us

--- front-end/src/aboutUs.js
+++ front-end/src/aboutUs.js
@@ -7,13 +7,115 @@ import { BrowserRouter as Router, Route, Link, Navlink, Switch } from 'react-rou
 
 function aboutUs(){
     return(
-        <div className="box">
-              <img src="assets\Coco\ Image\ background\ 3.png">
-              </img>
-              <h2>Manodya Mendis</h2>
-              <h3>2018629 </h3>
-              <h3>w1761956</h3>
+        
+        <div className="wholepage">
+             <nav className="navbar">
+
+<div className="nav">
+
+  <div className="nav-logo"> 
+
+    <h1 className ="AppName">Coco Measure</h1>
+    
+  </div> 
+
+</div>
+
+<div className="spacer"/>
+
+<div className="navContainer">
+
+  <ul className={"nav-menu"}>
+    <li className="nav-item">
+      <NavLink
+        exact
+        to="/"
+        className="nav-links"
+      >
+        Home
+      </NavLink>
+    </li>
+    <li className="nav-item">
+      <NavLink
+        exact
+        to="/Gazette"
+        className="nav-links"
+      >
+        Gazette
+      </NavLink>
+    </li>
+    <li className="nav-item">
+      <NavLink
+        exact
+        to="/aboutUs"
+        className="nav-links"
+      >
+        About us
+      </NavLink>
+    </li>
+
+  </ul>
+
+</div>
+</nav>
+
+
+
+            
+             <div className="box">
+            <img className="imgAbout1" />;
+            
+            <h1> <br />
+            Bhushitha Ekanayake<br />
+              2019156<br />w1761194</h1>
+              
           </div>
+
+          <div className="box">
+            <img className="imgAbout2" />;
+            
+            <h1> <br />
+            Nisandi Jayasuriya<br />
+            20191144<br />w1790017</h1>
+              
+          </div>
+
+          <div className="box">
+            <img className="imgAbout3" />;
+            
+            <h1> <br />
+              Manodya Mendis<br />
+              2018629<br />w1761956</h1>
+              
+          </div>
+          <div className="box">
+            <img className="imgAbout4" />;
+            
+            <h1> <br />
+            Youjith	Deelake<br />
+            2018137<br />w1714901</h1>
+              
+          </div>
+          <div className="box">
+            <img className="imgAbout5" />;
+            
+            <h1> <br />
+            Denuwan	Gunasinghe<br />
+              2018583<br />w1742255</h1>
+              
+          </div>
+
+          <div className="box">
+            <img className="imgAbout6" />;
+            
+            <h1> <br />
+            Pasal	Wickramasinghe<br />
+            2018277<br />w1715783</h1>
+              
+          </div>
+
+        </div>
+       
     );
 }
 
