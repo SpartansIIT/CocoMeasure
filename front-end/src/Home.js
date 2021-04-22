0af1337c5a7224937128effe8fabf7601ff3b2f8
Denuwan <denuwan.2018583@iit.ac.lk>
2021-04-22 13:54:45 +0530

    Add - changes on upload image

--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -31,7 +31,7 @@ function Home(){
                 <li className="nav-item">
                   <NavLink
                     exact
-                    to="/Home"
+                    to="/"
                     className="nav-links"
                   >
                     Home
